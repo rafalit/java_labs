14356a808b19ef77552b8c4be60e11bff8f7e241
rafalit <szarowiczrafal@gmail.com>
2024-04-06 19:14:15 +0200

    basket splits well so far, test's time

---
+++ BasketSplitter/src/main/java/com/ocado/basket/App.java
@@ -0,0 +1,28 @@
+package com.ocado.basket;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            String basketFilePath = "src/main/resources/basket-1.json";
+            List<String> basket = BasketLoader.loadBasket(basketFilePath);
+
+            String configFilePath = "src/main/resources/config.json";
+            BasketSplitter splitter = new BasketSplitter(configFilePath, basket);
+
+            Map<String, List<String>> deliveryGroups = splitter.split();
+
+            System.out.println("Grupy dostawy:");
+            for (Map.Entry<String, List<String>> entry : deliveryGroups.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+
+            } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ BasketSplitter/src/main/java/com/ocado/basket/Config.java
@@ -0,0 +1,13 @@
+package com.ocado.basket;
+
+import java.util.List;
+import java.util.Map;
+
+public class Config {
+    private Map<String, List<String>> deliveryOptions;
+
+    public Map<String, List<String>> getDeliveryOptions() {
+        return deliveryOptions;
+    }
+
+}
